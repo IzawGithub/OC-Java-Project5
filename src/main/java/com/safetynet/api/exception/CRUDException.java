6c01c8889b9290af2772d397f3c603c07ae3f42c
IzawGithub <IzawGithub@pm.me>
2024-06-24 11:59:00 +0200

    Replace equal and hashcode with lombok

--- src/main/java/com/safetynet/api/exception/CRUDException.java
+++ src/main/java/com/safetynet/api/exception/CRUDException.java
@@ -1,8 +1,10 @@
 package com.safetynet.api.exception;
 
 import java.text.MessageFormat;
-import java.util.Objects;
 
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = false)
 public class CRUDException extends Exception {
     public enum ECRUDException {
         ALREADY_EXIST,
@@ -12,25 +14,6 @@ public class CRUDException extends Exception {
     public CRUDException(ECRUDException exception, Object item) {
         super(MessageFormat.format("CRUDException: ''{0}'' (''{1}'')", exception, item));
         this.enumException = exception;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (Objects.isNull(obj))
-        {
-            return false;
-        }
-        if (this.getClass() != obj.getClass())
-        {
-            return false;
-        }
-
-        final var other = (CRUDException) obj;
-        return this.getMessage().equals(other.getMessage());
-    }
-    @Override
-    public int hashCode() {
-        return enumException.hashCode();
     }
 
     final ECRUDException enumException;
